8d0db7f1e16edbc47b773702108ec006b455f127
LearnCode2016 <padmasujah@yahoo.com>
2016-04-04 11:17:24 -0400

    Added a comment to the main activity and created a new class Test

--- app/src/main/java/supsud/helloworld/MainActivity.java
+++ app/src/main/java/supsud/helloworld/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //look this is the layout file I am missing
+        
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/supsud/helloworld/Test.java
@@ -0,0 +1,8 @@
+package supsud.helloworld;
+
+/**
+ * Created by Padmasujah on 4/4/2016.
+ */
+public class Test {
+    public String name;
+}
